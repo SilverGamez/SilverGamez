82bca869aa89f4150b38ff1c56d4b000c935c269
SilverGamez <79312009+SilverGamez@users.noreply.github.com>
2021-06-14 10:10:08 +1000

    question asker
    Asks 3 questions about you. Idk why i made this but yeah

---
+++ questionAsker.py
@@ -0,0 +1,5 @@
+question1 = input('Hey i am silver, whats your name?: ')
+question2 = input('I mainly like doing javascript, what about you?: ')
+question3 = input('My favourite game is subnautica, what is your favourite?: ')
+
+print(f'ABOUT YOU: Hey {question1}, you like to code using {question2}! You play {question3}, thats amazing!')
